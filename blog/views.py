18f57f4a8ad4710c409108504392ecf4a5990cb1
qumi-gen <buturipika57@gmail.com>
2021-06-15 15:10:45 +0900

    ビュー

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+"""
+post_list という関数（def から始まる部分のことです）を作りました。これは request を引数に取ります。
+blog/post_list.html テンプレートを（色々なものを合わせて）組み立てる render という関数を呼び出して得た値を return しています。
+"""
