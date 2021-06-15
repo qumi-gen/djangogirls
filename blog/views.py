83bdfb39c83877297eb4fba259253b90740e173c
qumi-gen <buturipika57@gmail.com>
2021-06-15 16:07:25 +0900

    views.py

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 """
 post_list という関数（def から始まる部分のことです）を作りました。これは request を引数に取ります。
